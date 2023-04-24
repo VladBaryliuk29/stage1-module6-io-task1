8e0c1eadb5e14ecdd34a93c55b6f71417eeed784
unknown <vladmain29@gmail.com>
2023-04-24 17:22:07 +0300

    It was really hard

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,22 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
-
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        try(FileInputStream reader1 = new FileInputStream(file)){
+            String information = "";
+            int ch;
+            while ((ch = reader1.read()) != -1) {
+                information += (char)ch;
+            }
+            String[] strArray = information.split("[ \n\r]");
+            Profile profile1 = new Profile(strArray[1], Integer.parseInt(strArray[4]), strArray[7], Long.parseLong(strArray[10]));
+            return profile1;
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+        }
+        return null;
     }
 }
